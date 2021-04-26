181915e8ed3e2b1ecfd72bdf43bdbfae2324c706
raphael-co <76869389+raphael-co@users.noreply.github.com>
2021-04-26 11:34:22 +0200

    recuperation register

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 
 import { HttpClient } from '@angular/common/http';
 
+import { UserRegister } from '../interfaces/user-register';
+
 
 @Injectable({
     providedIn: 'root'
@@ -17,6 +19,14 @@ export class AuthService {
             this.http.post(this.url + '/login', { email: email, password: password }).subscribe((data: any) => {
                 (!data.token) ? rejects(false): resolve(data);
               
+            });
+        });
+    }
+
+    register(user: UserRegister) {
+        return new Promise((resolve, rejects) => {
+            this.http.post(this.url + '/register', user).subscribe((data: any) => {
+                (!data.token) ? rejects(data.message): resolve(data);
             });
         });
     }
